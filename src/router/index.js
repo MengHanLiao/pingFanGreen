f39600c8f40bd202720f972c3b380719a28a038f
Menghal <s1102481217@gmail.com>
2022-03-24 14:33:44 +0800

    feat: 調整 router/index.js

--- src/router/index.js
+++ src/router/index.js
@@ -4,10 +4,62 @@ const routes = [
   {
     path: '/',
     component: () => import('../views/forestage/frontView.vue'),
+    children: [
+      {
+        path: '',
+        component: () => import('../views/forestage/HomeView.vue'),
+      },
+      {
+        path: 'product',
+        component: () => import('../views/forestage/ProductView.vue'),
+      },
+      {
+        path: 'article',
+        component: () => import('../views/forestage/ArticleView.vue'),
+      },
+      {
+        path: 'question',
+        component: () => import('../views/forestage/QuestionView.vue'),
+      },
+      {
+        path: 'cart',
+        component: () => import('../views/forestage/CartView.vue'),
+      },
+      {
+        path: 'favorite',
+        component: () => import('../views/forestage/FavoriteView.vue'),
+      },
+      {
+        path: 'customization',
+        component: () => import('../views/forestage/CustomizationView.vue'),
+      },
+    ],
   },
   {
     path: '/admin',
     component: () => import('../views/backstage/DashboardView.vue'),
+    children: [
+      {
+        path: 'home',
+        component: () => import('../views/backstage/AdminHome.vue'),
+      },
+      {
+        path: 'product',
+        component: () => import('../views/backstage/AdminProduct.vue'),
+      },
+      {
+        path: 'order',
+        component: () => import('../views/backstage/AdminOrder.vue'),
+      },
+      {
+        path: 'coupon',
+        component: () => import('../views/backstage/AdminCoupon.vue'),
+      },
+      {
+        path: 'article',
+        component: () => import('../views/backstage/AdminArticle.vue'),
+      },
+    ],
   },
   {
     path: '/login',
