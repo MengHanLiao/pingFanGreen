aa7a6114164cb1f4cb44446a8b0d3cfe1ce513f7
Menghal <s1102481217@gmail.com>
2022-03-29 16:42:23 +0800

    feat: 關於我們、系列商品

--- src/views/forestage/HomeView.vue
+++ src/views/forestage/HomeView.vue
@@ -63,25 +63,66 @@
         </template>
       </swiper>
     </section>
+    {{containerHeight}}
+    <section id="aboutUs">
+      <div class="container pt-8">
+        <div class="row">
+          <div class="col-lg-6 col-xl-5 offset-xl-1 text-center text-lg-start">
+            <h2 class="text-primary mb-6">關於平方綠</h2>
+            <h3 class="fs-4 mb-5">大家好，我們是平方綠</h3>
+            <p>因為養了仙人掌而開始被小多肉們迷得神魂顛倒，<br>
+            平方綠希望能讓每個人都成為多肉好盆友。</p>
+            <p>我們不只提供健康的多肉盆栽、客製化禮物，<br>
+            也推出一系列的養育心得文章，歡迎大家聯絡交流。</p>
+            <div class="mb-2 mb-lg-12">
+              <button class="btn btn-green-100 me-3" type="button">聯絡我們</button>
+              <a href="#" class="p-3"><img src="../../assets/images/icons/facebook.png"
+                style="width: 2rem" alt="fb"></a>
+              <a href="#" class="p-3"><img src="../../assets/images/icons/instagram.png"
+                style="width: 2rem" alt="ig"></a>
+            </div>
+          </div>
+          <div class="col-lg-6 align-self-end">
+            <div class="background-about"></div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section id="productCategory">
+      <div class="bg-green-50">
+        <div class="container pt-8">
+          <h2 class="text-center mb-6">系列商品</h2>
+          <div class="row gy-4">
+            <div class="col-md-4">
+              <router-link class="overlay" to="/product">
+                <img class="overlay-img object-fit-cover"
+                  src="../../assets/images/succulents_brownPots.jpg"
+                  style="height: 300px" alt="series1">
+                <p class="overlay-text text-white fs-4">仙人掌</p>
+              </router-link>
+            </div>
+            <div class="col-md-4">
+              <router-link class="overlay" to="/product">
+                <img class="overlay-img object-fit-cover"
+                  src="../../assets/images/bearSucculents.jpg"
+                  style="height: 300px" alt="series1">
+                <p class="overlay-text text-white fs-4">熊童子</p>
+              </router-link>
+            </div>
+            <div class="col-md-4">
+              <router-link class="overlay" to="/product">
+                <img class="overlay-img object-fit-cover"
+                  src="../../assets/images/flowerSucclents.jpg"
+                  style="height: 300px" alt="series1">
+                <p class="overlay-text text-white fs-4">花形多肉</p>
+              </router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
-
-<style lang="scss" scoped>
-.bounce{
-  animation: 1s ease-in-out infinite bouncing;
-}
-@keyframes bouncing{
-  0% {
-    transform: translate(0, 0);
-  }
-  50% {
-    transform: translate(0, 10px);
-  }
-  100% {
-    transform: translate(0, 0);
-  }
-}
-</style>
 
 <script>
 import { Navigation, Pagination, Autoplay } from 'swiper';
