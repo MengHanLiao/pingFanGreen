1a2bf3be3b5fc8d9757b737f585269186463450a
Menghal <s1102481217@gmail.com>
2022-03-30 23:16:57 +0800

    feat: 商品頁面 nav

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import { createApp } from 'vue';
+
+import 'bootstrap';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+
 import App from './App.vue';
 import router from './router';
-import 'bootstrap';
 
-createApp(App).use(router).mount('#app');
+const app = createApp(App);
+
+app.use(router);
+app.use(VueAxios, axios);
+
+app.mount('#app');
--- src/views/forestage/ProductView.vue
+++ src/views/forestage/ProductView.vue
@@ -1,5 +1,52 @@
 <template>
-  <div class="home">
-    產品
+  <div class="container pt-8 pb-12">
+    <h2 class="text-center mb-7">商品專區</h2>
+    <div class="row">
+      <div class="col">
+        <h5>商品分類</h5>
+        <ul class="nav flex-column">
+          <li class="nav-item" v-for="(category,index) in categories" :key="index+'dasdf'">
+            <router-link to="/product" class="nav-link">{{ category }}</router-link>
+          </li>
+        </ul>
+      </div>
+      <div class="col"></div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      categories: [],
+      products: [],
+      pagination: [],
+    };
+  },
+  methods: {
+    getData() {
+      this.$http.get(
+        `${process.env.VUE_APP_API_BASEURL}/api/${process.env.VUE_APP_PATH}/products`,
+      ).then((res) => {
+        this.products = res.data.products;
+        this.pagination = res.data.pagination;
+        this.findCategory();
+      }).catch((err) => {
+        console.dir(err);
+      });
+    },
+    findCategory() {
+      this.products.forEach((item) => {
+        const isExist = this.categories.some((category) => category === item.category);
+        if (!isExist) {
+          this.categories.push(item.category);
+        }
+      });
+    },
+  },
+  created() {
+    this.getData();
+  },
+};
+</script>
