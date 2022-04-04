fcb4f27a5d46542bed14ad9fe7fa586360c34cad
Menghal <s1102481217@gmail.com>
2022-04-04 17:39:28 +0800

    feat: Not found 頁面

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -4,7 +4,7 @@
     <div class="container text-center pt-7 pt-md-8 pb-12">
       <h2 class="mb-7 mb-md-7">404</h2>
       <p>您想找的頁面不存在</p>
-      <p class="mb-7">請重新確認想搜尋的項目或是回到首頁</p>
+      <p class="mb-7">請重新確認搜尋的項目或是回到首頁</p>
       <router-link to="/" class="btn btn-outline-dark mb-9">回到首頁</router-link>
       <img
         class="object-fit-cover mb-7"
