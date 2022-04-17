c3f07a221860b6412fa29f652f1a174274d72748
Menghal <s1102481217@gmail.com>
2022-04-17 22:47:28 +0800

    feat: 後臺版面新增

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import { createRouter, createWebHashHistory } from 'vue-router';
 const routes = [
   {
     path: '/',
-    component: () => import('../views/forestage/frontView.vue'),
+    component: () => import('../views/forestage/FrontView.vue'),
     children: [
       {
         path: '',
---
+++ src/views/backstage/AdminHome.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <p>後臺首頁</p>
+  </div>
+</template>
